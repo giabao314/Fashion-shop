ee044a8c2725f97ebf0a59e2af5458010b52c026
thepi314coding <giabao991199@gmail.com>
2022-04-19 15:27:07 +0700

    sperate nav-bar + js authentication

--- authHandling.php
+++ authHandling.php
@@ -1,10 +1,11 @@
 <?php
-    echo '<div> hien thi bang PHP</div>';
-    $conn = mysqli_connect("localhost", "root", "", "fashion-shop");
+    // echo '<div> hien thi bang PHP</div>';
+    // $conn = mysqli_connect("localhost", "root", "", "fashion-shop");
 
-    if($conn) {
-        echo 'ket noi thanh cong';
-    } else {
-        echo 'ket noi that bai';
-    }
+    // if($conn) {
+    //     echo 'ket noi thanh cong';
+    // } else {
+    //     echo 'ket noi that bai';
+    // }
+
 ?>
