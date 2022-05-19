3c5b44dfb64c071bc177dfa17fcba2164022caf8
giabao314 <giabao991199@gmail.com>
2022-05-19 13:17:16 +0700

    update show product

--- category.php
+++ category.php
@@ -1,6 +1,6 @@
 <?php
 include('opendb.php');
-$sql_category = mysqli_query($conn, 'SELECT * FROM loaisp ORDER BY idLoaiSP ASC');
+$sql_category_soLuong = mysqli_query($conn, 'SELECT sanpham.idLoaiSP,SUM(sanpham.slTonKho) as sum,loaisp.tenLoaiSP FROM sanpham,loaisp WHERE sanpham.idLoaiSP=loaisp.idLoaiSP GROUP BY idLoaiSP ASC');
 // if($category)
 // echo("ket noi thanh cong!");
 // echo $category;
@@ -8,15 +8,15 @@ $sql_category = mysqli_query($conn, 'SELECT * FROM loaisp ORDER BY idLoaiSP ASC'
 
 <div id="widget-collapse" class="category collapse show">
     <?php
-    while ($row_category = mysqli_fetch_array($sql_category)) {
+    while ($row_category = mysqli_fetch_array($sql_category_soLuong)) {
     ?>
         <a href="./product.php/?category=<?php echo $row_category['idLoaiSP']?>" class="filter-item">
             <div class="filter-name">
                 <?php echo $row_category['tenLoaiSP'] ?>
             </div>
-            <!-- <div class="filter-query">
-            <span class="item-count">10</span>
-        </div> -->
+            <div class="filter-query">
+            <span class="item-count"><?php echo $row_category['sum'] ?></span>
+        </div>
         </a>
     <?php
     }
---
+++ productItem.php
@@ -0,0 +1,54 @@
+<?php
+include('opendb.php');
+$sql_category = mysqli_query($conn, "SELECT * FROM loaisp");
+$sql_product = mysqli_query($conn, "SELECT * FROM sanpham, loaisp WHERE sanpham.idLoaiSP = loaisp.idLoaiSP");
+// $sql_product_cate = myqli_query($conn, "SELECT")
+$row_product
+?>
+<div class="row product-item-content">
+    <?php
+    while ($row_product = mysqli_fetch_array($sql_product)) {
+    ?>
+        <div class="col-product">
+            <figure class="product-header">
+                <a href="productDetail.php">
+                    <img src="./assets/img/product/<?php echo $row_product['anhSP']?>" alt="Product image" class="product-img">
+                </a>
+                <div class="product-action-vertical">
+                    <a href="#" class="btn-product-icon btn-wishlist btn-expand">
+                        <i class='bx bx-heart'></i>
+                        <span>Yêu thích</span>
+                    </a>
+                    <a href="#" class="btn-product-icon btn-quickview btn-expand">
+                        <i class='bx bxs-binoculars'></i>
+                        <span>Xem nhanh</span>
+                    </a>
+                    <a href="#" class="btn-product-icon btn-compare btn-expand">
+                        <i class='bx bx-shuffle'></i>
+                        <span>So sánh</span>
+                    </a>
+                </div>
+                <div class="product-action">
+                    <a href="cart.php" class="btn-product-cart">
+                        <i class='bx bx-cart-add'></i>
+                        <span> Thêm vào giỏ</span>
+                    </a>
+                </div>
+            </figure>
+            <div class="product-content">
+                <div class="product-cat">
+                    <a href="./product/?category=<?php echo $row_product['idLoaiSP'] ?>"><?php echo $row_product['tenLoaiSP'] ?></a>
+                </div>
+                <h3 class="product-title">
+                    <a href=""><?php echo $row_product['tenSP'] ?></a>
+                </h3>
+                <div class="product-price">
+                    <span><?php echo number_format($row_product['donGia']).'$' ?></span>
+                </div>
+            </div>
+        </div>
+    <?php
+    }
+    ?>
+
+</div>
